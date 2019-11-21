0887e39a198b9f76a398767794620308968c3b5e
agizzi <alexandra.n.gizzi@gmail.com>
2019-11-21 09:14:05 -0500

    working on fetching username info

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Notification from './Notifications'
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class NavBar extends Component {
     constructor(props) {
@@ -9,13 +9,13 @@ class NavBar extends Component {
 
     render() {
         return (
-            <div className = "navbar">
-                <h1 className = "nav"> Hi {this.props.name}! Welcome back to StayClose. </h1>
+            <div className="navbar">
+                <h1 className="nav"> Hi {this.props.username}! Welcome back to StayClose. </h1>
                 <nav>
                     <ul>
-                        <li className = "nav"> {this.props.name}'s Account </li>
-                        <li className = "nav"> Add Circle </li>
-                        <li className = "nav"> Notifications </li>
+                        <li className="nav"> {this.props.username}'s Account </li>
+                        <li className="nav"> Add Circle </li>
+                        <li className="nav"> Notifications </li>
                     </ul>
                 </nav>
                 <Notification />
