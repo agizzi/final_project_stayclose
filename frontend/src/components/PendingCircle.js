76602f7e3f8900224fc3ca6acabd313d60c2a60d
pjmendoz <pearl.j.mendoza@gmail.com>
2019-12-03 19:11:01 -0500

    styling stuff

--- frontend/src/components/PendingCircle.js
+++ frontend/src/components/PendingCircle.js
@@ -97,12 +97,16 @@ class PendingCircle extends Component {
 
   render() {
     return (
-      <div className="invite-circle">
+      <div>
         <Navbar username={this.state.username} />
+        <div className="invite-circle"> 
         <h3>You have been invited to join {this.state.circleName} by {this.state.adminUsername}</h3>
-        <h4> Would you like to join this circle?</h4>
-        <button type="button" onClick={this.handleAccept}>Yes</button>
-        <button type="button" onClick={this.handleDecline}>No</button>
+        <h4 className="pending"> Would you like to join this circle?</h4>
+        <div className="pending-buttons">
+          <button type="button" className="pend" onClick={this.handleAccept}>Yes</button>
+          <button type="button" className="pend" onClick={this.handleDecline}>No</button>
+        </div>
+        </div>
       </div >
 
     );
